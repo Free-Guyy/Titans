a73132babc2d4b767e7fe51e078b13d11734fb30
SiddamreddyPavankumarreddy <siddamreddypavankumarreddy143@gmail.com>
2020-09-11 09:37:37 +0530

    Intial Commit

--- Addition.java
+++ Addition.java
@@ -1,8 +1,12 @@
-public class Addition
-{
-	public int add(int a, int b)
-   {
-      int sum = a + b;
-      return sum;
-   }
+import java.util.Scanner;
+public class Addition {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+	int add = num1+num2;
+System.out.println("Addition of num1 & num2 is :"+add);
+	}
 }
--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,12 @@
-public class Multiply{
-public void mul(int a,int b){
-int product = 0;
-product = a*b;
-System.out.println("Product of two numbers is " + product);
-}
+import java.util.Scanner;
+public class Multiply {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+        int product = num1*num2;
+        System.out.println("Output: "+product);
+    }
 }
